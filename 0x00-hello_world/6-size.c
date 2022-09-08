6ca89f9117921cf7c4752a6db705c266d597d8f2
igee <igbagboleye@gmail.com>
2022-09-08 16:16:56 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 int main(void)
 
 {
-printf("Size of a char: %id byte(s)\n"), sizeof(char));
-printf("Size of an int: %id byte(s)\n", sizeof(int));
-printf("Size of a long int: %id byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %id byte(s)\n", sizeof(long long int));
-printf("Size of a float: %id byte(s)\n", sizeof(float));
+printf("Size of a char: %i byte(s)\n"), sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 
 }
