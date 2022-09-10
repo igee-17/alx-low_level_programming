aaf8324498ea515ec3d54aadf0b7c026ebb38d17
igee <igbagboleye@gmail.com>
2022-09-09 19:50:59 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - this is where the program start running
+ *Return:returns zero if error and non zero if no errors
+ */
+
+int main(void)
+{
+
+	int num;
+
+num = 0;
+
+while (num < 10)
+{
+	putchar(num);
+	num++;
+}
+	putchar('\n');
+
+	return (0);
+}
