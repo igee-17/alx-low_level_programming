104b91834076d73e3ead752377228b38d9a8f255
igee <igbagboleye@gmail.com>
2022-09-09 20:53:29 -0700

    add

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,9 +19,6 @@ int main(void)
 			if (i < j && i != j)
 			{
 				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
 
 				if (i != 98 || j != 99)
