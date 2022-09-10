e3372b48e330cd6d6cb33506b6d1390b3c1bbdf7
igee <igbagboleye@gmail.com>
2022-09-09 19:23:55 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - this is where the program start running
+ *Return:returns zero if error and non zero if no errors
+ */
+
+int main(void)
+{
+
+	int alphabet;
+
+	alphabet = 'a';
+
+	ALPHABET = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	while (ALPHABET <= 'Z')
+	{	
+		putchar(ALPHABET);
+		ALPHABET++;
+	}
+	putchar('\n');
+
+	return (0);
+}
