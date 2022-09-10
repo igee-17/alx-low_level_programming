3d4a3fa6c373629d3a4545b8e98384e2d3fd17b3
igee <igbagboleye@gmail.com>
2022-09-09 21:07:52 -0700

    add

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,6 +10,7 @@ int main(void)
 {
 
 	int c, j;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (j = '0'; j <= '9'; j++)
@@ -22,8 +23,7 @@ int main(void)
 				if (c != '8' || (c == '8' && j != '9'))
 				{
 					putchar(',');
-					putchar(' ');
-				
+					putchar(' ');				
 				}
 			}
 		}
