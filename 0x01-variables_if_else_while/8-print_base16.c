dee4ca435b90f24149907f69ed2d2fde38b73f08
igee <igbagboleye@gmail.com>
2022-09-09 20:12:39 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - this is where the program start running
+ *Return:returns zero if error and non zero if no errors
+ */
+
+int main(void)
+{
+
+	char c;
+
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++
+	}
+
+	for(c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
