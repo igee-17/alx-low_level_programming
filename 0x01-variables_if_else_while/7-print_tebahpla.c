6de5051dac808d385d64f4a5dedd65eae25515d4
igee <igbagboleye@gmail.com>
2022-09-09 20:06:10 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - this is where the program start running
+ *Return:returns zero if error and non zero if no errors
+ */
+
+int main(void)
+{
+
+int alphabet;
+
+alphabet = 'z';
+
+while (alphabet > 'a')
+{
+	putchar(alphabet);
+	alphabet--;
+}
+	putchar('\n');
+
+	return (0);
+}
