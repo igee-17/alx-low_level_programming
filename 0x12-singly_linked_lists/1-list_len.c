c50abd57b56ea315d473635fca2105333f50e2de
igee <igbagboleye@gmail.com>
2022-10-21 01:49:09 -0700

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
+size_t nelem;
 
-while (h)
+nelem = 0;
+while (h != NULL)
 {
-	elements++;
 	h = h->next;
+	nelem++;
 }
-
-return (elements);
+return (nelem);
 }
